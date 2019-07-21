5473d2c7b2ff9f94f89f768aa1f4fd26908b6bd0
jaironsousa1 <jaironsousa@gmail.com>
2019-07-21 09:31:33 -0300

    Criação do aplicação para testes unitarios.

---
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTeste.java
@@ -0,0 +1,38 @@
+package br.ce.wcaquino.servicos;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Date;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.Test;
+
+import br.ce.wcaquino.entidades.Filme;
+import br.ce.wcaquino.entidades.Locacao;
+import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.utils.DataUtils;
+
+public class LocacaoServiceTeste {
+	@Test
+	public void teste() {
+		// Cenário
+		LocacaoService service = new LocacaoService();
+		Usuario usuario = new Usuario("Usuario 1");
+		Filme filme = new Filme("Filme 1", 2, 5.0);
+
+		// ação
+		Locacao locacao = service.alugarFilme(usuario, filme);
+		
+		// Verificação
+		assertThat(locacao.getValor(), is(equalTo(5.0)));
+		assertThat(locacao.getValor(), is(not(6.0)));
+		assertThat(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()),is(true));
+		assertThat(DataUtils.isMesmaData(locacao.getDataRetorno(), 
+				DataUtils.obterDataComDiferencaDias(1)), is(true));
+
+	}
+}
