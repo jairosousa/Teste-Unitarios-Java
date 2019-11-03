725dbaaeda4d15aa91cdd2be42112a56a957984f
jaironsousa1 <jaironsousa@gmail.com>
2019-11-03 18:26:28 -0300

    34 - Spy

--- src/main/java/br/ce/wcaquino/servicos/Calculadora.java
+++ src/main/java/br/ce/wcaquino/servicos/Calculadora.java
@@ -18,5 +18,10 @@ public class Calculadora {
 		}
 		return a / b;
 	}
+	
+	
+	public void imprime() {
+		System.out.println("Passei aqui");
+	}
 
 }
--- src/test/java/br/ce/wcaquino/servicos/CalculadoraMockTest.java
+++ src/test/java/br/ce/wcaquino/servicos/CalculadoraMockTest.java
@@ -1,11 +1,43 @@
 package br.ce.wcaquino.servicos;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 
 public class CalculadoraMockTest {
+
+	@Mock
+	private Calculadora calcMock;
+
+	@Spy
+	private Calculadora calcSpy;
+
+	@Before
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+	}
+
+	@Test
+	public void devoMostrarDiferencaEntreMockSpy() {
+
+		Mockito.when(calcMock.somar(1, 2)).thenReturn(8);
+		Mockito.when(calcSpy.somar(1, 2)).thenReturn(8);
+		Mockito.doReturn(5).when(calcSpy).somar(1, 2);
+
+		System.out.println("Mock: " + calcMock.somar(1, 5));
+		System.out.println("Spy: " + calcSpy.somar(1, 5));
+		
+		System.out.println("Mock");
+		calcMock.imprime();
+		System.out.println("Spy");
+		calcSpy.imprime();
+
+	}
 
 	@Test
 	public void teste() {
@@ -16,8 +48,8 @@ public class CalculadoraMockTest {
 		Mockito.when(calc.somar(argCapt.capture(), argCapt.capture())).thenReturn(5);
 
 		Assert.assertEquals(5, calc.somar(1, 2));
-		
-		System.out.println(argCapt.getAllValues());
+
+//		System.out.println(argCapt.getAllValues());
 	}
 
 }
