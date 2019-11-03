50b90615f818e4962ab0cb30ae4637ad670ff974
jaironsousa1 <jaironsousa@gmail.com>
2019-11-03 16:03:05 -0300

    30 - Verificanto comportamento parte 2

--- src/test/java/br/ce/wcaquino/servicos/CalculadoraMockTest.java
+++ src/test/java/br/ce/wcaquino/servicos/CalculadoraMockTest.java
@@ -8,7 +8,7 @@ public class CalculadoraMockTest {
 	@Test
 	public void teste() {
 		Calculadora calc = Mockito.mock(Calculadora.class);
-		Mockito.when(calc.somar(Mockito.anyInt(), Mockito.anyInt())).thenReturn(5);
+		Mockito.when(calc.somar(Mockito.eq(1), Mockito.anyInt())).thenReturn(5);
 		
 		System.out.println(calc.somar(1, 2));
 	}
