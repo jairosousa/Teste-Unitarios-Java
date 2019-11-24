275be76a3689e91df72573546eba0c4894f0f38d
jaironsousa1 <jaironsousa@gmail.com>
2019-11-24 11:15:15 -0300

    Seção 2: Junit 17 - @Ignore e Assumptions

---
+++ 02 - JUnit/17 - @Ignore e Assumptions/src/test/java/br/ce/wcaquino/services/LocacaoServiceTeste.java
@@ -0,0 +1,203 @@
+package br.ce.wcaquino.services;
+
+import static br.ce.wcaquino.utils.DataUtils.isMesmaData;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Assume;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+import org.junit.rules.ExpectedException;
+
+import br.ce.wcaquino.entidades.Filme;
+import br.ce.wcaquino.entidades.Locacao;
+import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.exceptions.FilmeSemEstoqueException;
+import br.ce.wcaquino.exceptions.LocadoraException;
+import br.ce.wcaquino.utils.DataUtils;
+
+public class LocacaoServiceTeste {
+
+	/**
+	 * Não deve devolver filmes no domingo.
+	 */
+
+	/**
+	 * DESCONTOS CRESCENTES 25% no 3º filme 50% no 4º filme 75% no 5º filme 100% no
+	 * 6º filme
+	 */
+
+	/**
+	 * Roles é artificio recente no JUnit ele permite alterar alguns comportamentos
+	 * dos testes, é possivel criar regras proprias também
+	 */
+	@Rule
+	public ErrorCollector error = new ErrorCollector();
+
+	@Rule
+	public ExpectedException exception = ExpectedException.none();
+
+	private LocacaoService service;
+
+	@Before
+	public void setup() {
+		service = new LocacaoService();
+	}
+
+	@Test
+	public void deveAlugarFilme() throws Exception {
+		Assume.assumeFalse(DataUtils.verificarDiaSemana(new Date(), Calendar.SATURDAY));
+
+		// Cenário -> onde as variavéis são inicializadas
+		Usuario usuario = new Usuario("Usuario 1");
+		List<Filme> filmes = Arrays.asList(new Filme("Filme 1", 2, 5.0));
+
+		// Ação -> onde invocamos o metodo que vamos testar
+		Locacao locacao = service.alugarFilme(usuario, filmes);
+
+		// Validação -> onde vamos coletar o resiltado da ação com o cenário
+		error.checkThat(locacao.getValor(), is(equalTo(5.0)));
+		error.checkThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
+		error.checkThat(isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), is(true));
+
+	}
+
+	/**
+	 * 1ª forma: ELEGANTE
+	 * 
+	 * @throws Exception
+	 */
+	@Test(expected = FilmeSemEstoqueException.class)
+	public void naoDeveAlugarFilmeSemEstoque() throws Exception {
+		// Cenário -> onde as variavéis são inicializadas
+		Usuario usuario = new Usuario("Usuario 1");
+		List<Filme> filmes = Arrays.asList(new Filme("Filme 1", 0, 5.0));
+
+		// Ação -> onde invocamos o metodo que vamos testar
+		service.alugarFilme(usuario, filmes);
+
+	}
+
+	@Test
+	public void naoDeveAlugarFilmeSemUsuario() throws FilmeSemEstoqueException {
+
+		// Cenário
+		List<Filme> filmes = Arrays.asList(new Filme("Filme 1", 2, 4.0));
+
+		// Ação
+		try {
+			service.alugarFilme(null, filmes);
+			Assert.fail();
+		} catch (LocadoraException e) {
+			assertThat(e.getMessage(), is("Usuario vazio"));
+		}
+
+	}
+
+	@Test
+	public void naoDeveAlugarFilmeSemFilme() throws FilmeSemEstoqueException, LocadoraException {
+
+		// Cenário
+		Usuario usuario = new Usuario("Usuario 1");
+
+		exception.expect(LocadoraException.class);
+		exception.expectMessage("Filme vazio");
+
+		// Ação
+		service.alugarFilme(usuario, null);
+
+		System.out.println("Forma Nova");
+	}
+
+	@Test
+	public void devePagar75PctNoFilme3() throws FilmeSemEstoqueException, LocadoraException {
+
+		// Cenário
+		Usuario usuario = new Usuario("Usuario 1");
+		List<Filme> filmes = Arrays.asList(new Filme("Filme 1", 2, 4.0), new Filme("Filme 2", 2, 4.0),
+				new Filme("Filme 3", 2, 4.0));
+
+		// Ação
+		Locacao resultado = service.alugarFilme(usuario, filmes);
+
+		// Verificação 4+4+3
+		assertThat(resultado.getValor(), is(11.0));
+
+	}
+
+	@Test
+	public void devePagar50PctNoFilme4() throws FilmeSemEstoqueException, LocadoraException {
+
+		// Cenário
+		Usuario usuario = new Usuario("Usuario 1");
+		List<Filme> filmes = Arrays.asList(new Filme("Filme 1", 2, 4.0), new Filme("Filme 2", 2, 4.0),
+				new Filme("Filme 3", 2, 4.0), new Filme("Filme 4", 2, 4.0));
+
+		// Ação
+		Locacao resultado = service.alugarFilme(usuario, filmes);
+
+		// Verificação 4+4+3+2
+		assertThat(resultado.getValor(), is(13.0));
+
+	}
+
+	@Test
+	public void devePagar25PctNoFilme5() throws FilmeSemEstoqueException, LocadoraException {
+
+		// Cenário
+		Usuario usuario = new Usuario("Usuario 1");
+		List<Filme> filmes = Arrays.asList(new Filme("Filme 1", 2, 4.0), new Filme("Filme 2", 2, 4.0),
+				new Filme("Filme 3", 2, 4.0), new Filme("Filme 4", 2, 4.0), new Filme("Filme 5", 2, 4.0));
+
+		// Ação
+		Locacao resultado = service.alugarFilme(usuario, filmes);
+
+		// Verificação 4+4+3+2+1
+		assertThat(resultado.getValor(), is(14.0));
+
+	}
+
+	@Test
+	public void devePagar0PctNoFilme6() throws FilmeSemEstoqueException, LocadoraException {
+
+		// Cenário
+		Usuario usuario = new Usuario("Usuario 1");
+		List<Filme> filmes = Arrays.asList(new Filme("Filme 1", 2, 4.0), new Filme("Filme 2", 2, 4.0),
+				new Filme("Filme 3", 2, 4.0), new Filme("Filme 4", 2, 4.0), new Filme("Filme 5", 2, 4.0),
+				new Filme("Filme 6", 2, 4.0));
+
+		// Ação
+		Locacao resultado = service.alugarFilme(usuario, filmes);
+
+		// Verificação 4+4+3+2+1+0
+		assertThat(resultado.getValor(), is(14.0));
+
+	}
+
+	@Test
+	public void deveDevolverNaSegundaAoAlugarNoSabado() throws FilmeSemEstoqueException, LocadoraException {
+		
+		Assume.assumeTrue(DataUtils.verificarDiaSemana(new Date(), Calendar.SATURDAY));
+
+		// Cenário -> 
+		Usuario usuario = new Usuario("Usuario 1");
+		List<Filme> filmes = Arrays.asList(new Filme("Filme 1", 2, 5.0));
+
+		// Ação -> onde invocamos o metodo que vamos testar
+		Locacao retorno = service.alugarFilme(usuario, filmes);
+		
+		boolean ehSegunda = DataUtils.verificarDiaSemana(retorno.getDataRetorno(), Calendar.MONDAY);
+		Assert.assertTrue(ehSegunda);
+
+	}
+
+}
