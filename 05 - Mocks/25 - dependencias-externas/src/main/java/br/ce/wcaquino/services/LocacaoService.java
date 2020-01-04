d734aa111f5d2b0d9a35b683a305bb759f75bfd7
Jairo Nascimento <jaironsousa@gmail.com>
2020-01-04 17:22:09 -0300

    Seção 05 - Mocks: 25 - Dependências externas

--- 05 - Mocks/25 - dependencias-externas/src/main/java/br/ce/wcaquino/services/LocacaoService.java
+++ 05 - Mocks/25 - dependencias-externas/src/main/java/br/ce/wcaquino/services/LocacaoService.java
@@ -6,6 +6,7 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
+import br.ce.wcaquino.dao.LocacaoDAO;
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
@@ -14,6 +15,8 @@ import br.ce.wcaquino.exceptions.LocadoraException;
 import br.ce.wcaquino.utils.DataUtils;
 
 public class LocacaoService {
+	
+	private LocacaoDAO dao;
 
 	public Locacao alugarFilme(Usuario usuario, List<Filme> filmes) throws FilmeSemEstoqueException, LocadoraException {
 
@@ -78,7 +81,7 @@ public class LocacaoService {
 		locacao.setDataRetorno(dataEntrega);
 
 		// Salvando a locacao...
-		// TODO adicionar método para salvar
+		dao.salvar(locacao);
 
 		return locacao;
 	}
