b123155d2aa029284a3efe0b21f23c42e73019ef
Jairo Nascimento <jaironsousa@gmail.com>
2020-01-13 16:27:20 -0300

    Seção 05 - 30. Verificando Comportamentos parte 2

---
+++ 05 - Mocks/30 - verificando comportamentos - Parte 2/src/main/java/br/ce/wcaquino/services/LocacaoService.java
@@ -0,0 +1,120 @@
+package br.ce.wcaquino.services;
+
+import static br.ce.wcaquino.utils.DataUtils.adicionarDias;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import br.ce.wcaquino.dao.LocacaoDAO;
+import br.ce.wcaquino.entidades.Filme;
+import br.ce.wcaquino.entidades.Locacao;
+import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.exceptions.FilmeSemEstoqueException;
+import br.ce.wcaquino.exceptions.LocadoraException;
+import br.ce.wcaquino.utils.DataUtils;
+
+public class LocacaoService {
+
+	private LocacaoDAO dao;
+
+	private SPCService spcServioce;
+
+	private EmailService emailService;
+
+	public Locacao alugarFilme(Usuario usuario, List<Filme> filmes) throws FilmeSemEstoqueException, LocadoraException {
+
+		/**
+		 * Usuario e Filme são obrigatórios
+		 */
+		if (usuario == null) {
+			throw new LocadoraException("Usuario vazio");
+		}
+
+		if (filmes == null || filmes.isEmpty()) {
+			throw new LocadoraException("Filme vazio");
+		}
+
+		/**
+		 * Não deve alugar filme sem estoque Se tiver sem estoque um exeção será lançada
+		 */
+
+		for (Filme filme : filmes) {
+			if (filme.getEstoque() == 0) {
+				throw new FilmeSemEstoqueException();
+			}
+		}
+
+		if (spcServioce.possuiNegativacao(usuario)) {
+			throw new LocadoraException("Usuario negativado");
+		}
+
+		Locacao locacao = new Locacao();
+		locacao.setFilmes(filmes);
+		locacao.setUsuario(usuario);
+		locacao.setDataLocacao(new Date());
+
+		Double valorToral = 0d;
+		for (int i = 0; i < filmes.size(); i++) {
+			Filme filme = filmes.get(i);
+			Double valorFilme = filme.getPrecoLocacao();
+			switch (i) {
+			case 2:
+				valorFilme = valorFilme * 0.75;
+				break;
+			case 3:
+				valorFilme = valorFilme * 0.5;
+				break;
+			case 4:
+				valorFilme = valorFilme * 0.25;
+				break;
+			case 5:
+				valorFilme = valorFilme * 0;
+				break;
+			}
+
+			valorToral += valorFilme;
+		}
+
+		locacao.setValor(valorToral);
+
+		// Entrega no dia seguinte
+		Date dataEntrega = new Date();
+		dataEntrega = adicionarDias(dataEntrega, 1);
+
+		if (DataUtils.verificarDiaSemana(dataEntrega, Calendar.SUNDAY)) {
+			dataEntrega = adicionarDias(dataEntrega, 1);
+		}
+
+		locacao.setDataRetorno(dataEntrega);
+
+		// Salvando a locacao...
+		dao.salvar(locacao);
+
+		return locacao;
+	}
+
+	public void notificarAtrasos() {
+		List<Locacao> locacoes = dao.obterLocacoesPendentes();
+		for (Locacao locacao : locacoes) {
+			if (locacao.getDataRetorno().before(new Date())) {
+				emailService.notificarAtrasos(locacao.getUsuario());
+				
+			}
+		}
+
+	}
+
+	public void setEmailService(EmailService emailService) {
+		this.emailService = emailService;
+	}
+
+	public void setLocacaoDao(LocacaoDAO dao) {
+		this.dao = dao;
+	}
+
+	public void setSpcServioce(SPCService spcService) {
+		this.spcServioce = spcService;
+	}
+
+}
