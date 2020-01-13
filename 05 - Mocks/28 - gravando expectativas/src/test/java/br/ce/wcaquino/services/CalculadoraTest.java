a19d558751a06ef6b4e53135bac271ff7bbcef63
Jairo Nascimento <jaironsousa@gmail.com>
2020-01-13 13:16:51 -0300

    Seção 05 - 28 - Gravando Expectativas

---
+++ 05 - Mocks/28 - gravando expectativas/src/test/java/br/ce/wcaquino/services/CalculadoraTest.java
@@ -0,0 +1,80 @@
+package br.ce.wcaquino.services;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import br.ce.wcaquino.exceptions.NaoPodeDividirPorZeroException;
+
+public class CalculadoraTest {
+	
+	private Calculadora calc;
+	
+	@Before
+	public void setup() {
+		calc = new Calculadora();
+	}
+
+	@Test
+	public void deveSomarDoisValores() {
+		
+		// Cenário
+		int a = 5;
+		int b = 3;
+		
+		calc = new Calculadora();
+		
+		//Verificação
+		int resultado = calc.somar(a, b);
+		
+		Assert.assertEquals(8, resultado);
+		
+	}
+	
+	@Test
+	public void deveSubtrairDoisValores() {
+		
+		// Cenário
+		int a = 8;
+		int b = 5;
+		
+		calc = new Calculadora();
+		
+		//Verificação
+		int resultado = calc.subtrair(a, b);
+		
+		Assert.assertEquals(3, resultado);
+		
+	}
+	
+	@Test
+	public void deveDividirDoisValores() throws NaoPodeDividirPorZeroException {
+		
+		// Cenário
+		int a = 8;
+		int b = 4;
+		
+		// Ação
+		int resultado = calc.dividir(a, b);
+		
+		//Verificação
+		
+		Assert.assertEquals(2, resultado);
+		
+	}
+	
+	@Test(expected = NaoPodeDividirPorZeroException.class)
+	public void deveLancarExcecaoDividirPorZero() throws NaoPodeDividirPorZeroException {
+		
+		// Cenário
+		int a = 10;
+		int b = 0;
+		
+		// Ação
+		calc.dividir(a, b);
+		
+		//Verificação
+		
+	}
+
+}
