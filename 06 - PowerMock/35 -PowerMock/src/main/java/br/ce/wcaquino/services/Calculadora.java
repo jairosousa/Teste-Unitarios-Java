6856986781c3a11c8c9c5550ac0b823980392c6d
Jairo Nascimento <jaironsousa@gmail.com>
2020-01-14 11:22:53 -0300

    Seção 06: PowerMock 35 - PowerMock

---
+++ 06 - PowerMock/35 -PowerMock/src/main/java/br/ce/wcaquino/services/Calculadora.java
@@ -0,0 +1,27 @@
+package br.ce.wcaquino.services;
+
+import br.ce.wcaquino.exceptions.NaoPodeDividirPorZeroException;
+
+public class Calculadora {
+
+	public int somar(int a, int b) {
+		System.out.println("Executando metodo somar");
+		return a + b;
+	}
+
+	public int subtrair(int a, int b) {
+		return a - b;
+	}
+
+	public int dividir(int a, int b) throws NaoPodeDividirPorZeroException {
+		if (b == 0) {
+			throw new NaoPodeDividirPorZeroException();
+		}
+		return a / b;
+	}
+	
+	public void imprime() {
+		System.out.println("Passei aqui");
+	}
+
+}
